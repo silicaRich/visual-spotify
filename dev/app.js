a83dbd899c723b0063295e8902311d1bc05082d6
Nicole Flokos <nicole.flokos@gmail.com>
2017-06-05 21:10:35 -0400

    Reinitalize project with Webpack bundler.

---
+++ dev/app.js
@@ -0,0 +1,10 @@
+﻿require('angular'); // This tells webpack that we want to bundle Angular with our js
+
+
+var app = angular.module("app", []);
+
+app.controller("mainController", function ($scope) {
+
+    alert("greetings~");
+
+});
